a86250a5ca498e3bbc437644bdcb33927459515f
Tarek Clarke <79656799+tarek-clarke@users.noreply.github.com>
2025-06-08 17:57:26 -0400

    Synchronizing job import from AWS Glue at 2025/06/08 21:57:26

--- import/import.py
+++ import/import.py
@@ -14,7 +14,7 @@ df_customer = glueContext.spark_session.read.json(s3_customer_path)
 df_step = glueContext.spark_session.read.json(s3_step_path)
 df_accelerometer = glueContext.spark_session.read.json(s3_accelerometer_path)
 
-print("All dataframes loaded successfully.")
+# print("All dataframes loaded successfully.")
 
 
 # Show sample data
